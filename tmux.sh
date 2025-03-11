9f239433a879ed9b96def4267215b06595b46ce4
Mikhail Sherstennikov <mike.sherstennikov@gmail.com>
2025-03-11 13:55:36 +0200

    add atuin

--- tmux.sh
+++ tmux.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 tmux new-session -d -s main irb
-tmux neww -c /home/mike/proj/ -n "Trunk"
-tmux neww -c /home/mike/bld/ -n "Build"
-tmux neww -c /home/mike/bld/ # "mc"
-tmux neww -c /home/mike/bld/ # 4 
+tmux neww -c /home/mike/proj/nil -n "Trunk"
+tmux neww -c /home/mike/
+tmux neww -c /home/mike/
+tmux neww -c /home/mike/
 tmux neww # 5
 tmux neww # 6
 tmux neww # 7
 tmux neww # 8
-# 9
-tmux neww -c /home/mike/src 
+tmux neww -c /home/mike/proj/nil  -n "Trunk-2"
 
 tmux neww jupyter notebook
 
